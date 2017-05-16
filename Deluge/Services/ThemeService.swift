69c9179ae5f4169160d774e98b77b4cf21565354
yotamoo <ohayon.yotam@gmail.com>
2017-05-16 18:16:53 +0300

    fixed stupid theme service bug

--- Deluge/Services/ThemeService.swift
+++ Deluge/Services/ThemeService.swift
@@ -34,9 +34,9 @@ class ThemeManager: ThemeManaging/*, ThemeServicing*/ {
 
     func color(forTorrentState state: TorrentState) -> UIColor {
         switch state {
-        case .seeding: return UIColor(red: 0.97, green: 0.193, blue: 0.27, alpha: 1)
+        case .seeding: return UIColor(red: 0.194, green: 0.84, blue: 0.47, alpha: 1)
         case .paused: return UIColor(red: 0.153, green: 0.153, blue: 0.153, alpha: 1)
-        case .error: return UIColor(red: 0.194, green: 0.84, blue: 0.47, alpha: 1)
+        case .error: return UIColor(red: 0.97, green: 0.193, blue: 0.27, alpha: 1)
         case .downloading: return UIColor(red: 0.101, green: 0.161, blue: 0.216, alpha: 1)
         case .queued: return UIColor(red: 0.153, green: 0.153, blue: 0.153, alpha: 1)
         case .checking: return UIColor(red: 0.153, green: 0.153, blue: 0.153, alpha: 1)
